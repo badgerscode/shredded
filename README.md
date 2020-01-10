92f7047c0ba4e6e31b706ff97b59ebaae105c62f
Rafael Ahrons <rafael.ahrons@gmail.com>
2020-01-10 14:08:29 -0300

    Updated README and models

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # shredded-
 a workout reviewer and scheduller that you can use to track your workouts
+
+## How to configure your local environment
+1. Using virtual environmet:
+
+    To create on virtual environment in windows:
+    ```
+    python -m venv venv
+    ```
+    The second venv will be the name of the environment. After that, you must activate.
+    ```
+    .\venv\Scripts\activate
+    ```
+    To deactivate:
+    ```
+    deactivate
+    ```
+
+    After that you run step 2.
+
+1. Running pip install to install requirements file:
+    
+    You must run pip command to install project dependencies from the project.
+    ```
+    pip install requirements.txt
+    ```
--- app/models.py
+++ app/models.py
@@ -3,10 +3,11 @@ from app import db
 class Workout(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     owner = db.Column(db.String(64), index = True, unique = True)
+    name = db.column(db.String(64))
     exercises = db.relationship('Exercise', backref='workouts', lazy='dynamic')
-
+    
     def __repr__(self):
-        return '<Workout {}>'.format(self.owner)
+        return '<Workout {} {} {}>'.format(self.id, self.owner, self.name)
 
 class Exercise(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -14,4 +15,4 @@ class Exercise(db.Model):
     workout_id = db.Column(db.Integer, db.ForeignKey('workout.id'))
 
     def __repr__(self):
-        return '<Exercise {}>'.format(self.description)
+        return '<Exercise {} {}>'.format(self.id, self.description)
