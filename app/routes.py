b0e895bd6f5c727873b1652affd2dc99a800a4c6
Rafael Ahrons <rafael.ahrons@gmail.com>
2020-01-13 22:18:01 -0300

    Add route to add workout

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,12 @@
-from app import app
+from app import app, request, jsonify
 
 @app.route('/')
 @app.route('/hello')
 def helloworld():
     return "Hello to Schredded"
+
+@app.route('/addworkout', methods=['POST'])
+def add_workout():
+    data = request.get_json()
+    
+    return jsonify(data)
