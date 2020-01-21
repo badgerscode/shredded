86db4a484f291928f13f98b23172ec651903a3a6
Rafael Ahrons <rafael.ahrons@gmail.com>
2020-01-21 10:12:27 -0300

    Add add_workout to WorkoutService

--- app/routes.py
+++ app/routes.py
@@ -2,49 +2,39 @@ from app import app, request, jsonify, db
 from app.services import WorkoutService
 from app.models import Workout, Exercise
 
+
 @app.route('/')
 @app.route('/hello')
 def helloworld():
     return "Hello to Schredded"
 
+
 @app.route('/addworkout', methods=['POST'])
-def add_workout():    
+def add_workout():
     try:
-        data = request.get_json()    
-        
-        workout = Workout(owner = data["owner"], name = data["name"])    
-        db.session.add(workout)
-        
-        exercises = data["exercises"]
-        for element in exercises:
-            exercise = Exercise(description = element["description"], workouts = workout)
-            db.session.add(exercise)
-        
-        db.session.commit()    
-        print(workout.exercises.all())
+        data = request.get_json()
 
-        json = {
-            'id': workout.id,
-            'owner': workout.owner,
-            'name': workout.name
-        }
+        if ('owner' in data) and ('name' in data) and ('exercises' in data):            
+            service = WorkoutService()
+            response = jsonify(service.add_workout(data))
+            response.status_code = 200
 
-        response = jsonify(json)
-        response.status_code = 200    
-        
-        return response
+            return response
+        else:
+            raise Exception("Wrong values passed to the endpoint")
     except Exception as error:
         response = jsonify(error)
-        response.status_code = 500
+        response.status_code = 400
         return response
 
-@app.route('/getworkoutfrom/', methods = ['GET'])
+
+@app.route('/getworkoutfrom/', methods=['GET'])
 def get_workout_from():
     owner = request.args.get('owner')
-    if owner is not None and owner :       
+    if owner is not None and owner:
         service = WorkoutService()
         response = jsonify(service.get_workout_from(owner))
         response.status_code = 200
         return response
-    else:    
+    else:
         return 'Bad request. Need to informe owner of the workout', 400
